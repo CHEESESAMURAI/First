9c93d82eb17c37a5ec8595b8b3298a1cf1526944
whalecore <whal3core@gmail.com>
2021-05-15 14:20:37 +0300

    added back btn

--- handlers/test.py
+++ handlers/test.py
@@ -1,12 +1,11 @@
-from aiogram.types.callback_query import CallbackQuery
 from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
-
+from aiogram.types.callback_query import CallbackQuery
+from keyboards import start_kb, test_kb
 from loader import bot, dp
-from misc import a, answers
-from keyboards import test_kb
+from misc import a, answers, quiz_analizer
 from states import Quiz
-from misc import quiz_analizer
+
 
 @dp.callback_query_handler(lambda query: query.data in ['start'], state=Quiz.started_test)
 async def test_entry(call: CallbackQuery, state: FSMContext):
@@ -16,6 +15,7 @@ async def test_entry(call: CallbackQuery, state: FSMContext):
     await bot.edit_message_text(text=a[0], chat_id=call.message.chat.id, message_id=call.message.message_id)
     await bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=test_kb)
     await Quiz.in_test.set()
+
 
 @dp.callback_query_handler(lambda query: query.data in ["+", "-"], state=Quiz.in_test)
 async def quiz(call: CallbackQuery, state: FSMContext):
@@ -31,3 +31,11 @@ async def quiz(call: CallbackQuery, state: FSMContext):
         await bot.edit_message_reply_markup(chat_id=chat_id, message_id=call.message.message_id, reply_markup=None)
         await bot.edit_message_text(quiz_analizer(answers[chat_id]), chat_id=chat_id, message_id=call.message.message_id)
         answers[chat_id] = ""
+
+
+@dp.callback_query_handler(lambda query: query.data in ["back"], state="*")
+async def back(call: CallbackQuery, state: FSMContext):
+    call.answer()
+    await bot.edit_message_text(text="Сейчас тут будут вопросы", message_id=call.message.message_id, chat_id=call.from_user.id)
+    await bot.edit_message_reply_markup(call.from_user.id, call.message.message_id, reply_markup=start_kb)
+    await state.reset_state()
